dc2afb532d6c4b1615ce88558c478a11ff0f413f
Ryan Houser <r.houser90@gmail.com>
2019-05-14 20:08:33 -0400

    ”Job Role” section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const nameInput = $('#name');
+const otherTitle = $('#other-title');
 
 // First text field in focus on page load
 nameInput.focus();
+// Hides otherTitle text field
+otherTitle.hide();
 
